3a14b7a8a0b92254d1625d3a3bb1978527248901
Luc Juggery <luc.juggery@gmail.com>
2017-09-17 16:13:55 +0200

    cleanup configuration

--- bin/start.sh
+++ bin/start.sh
@@ -5,5 +5,7 @@
 # - tornado server
 
 celery worker -Ofair --without-gossip --autoscale=4,1 --logfile=/var/log/celery/%h%I.log --loglevel=INFO --app=taskqueue 
-&& celery beat --loglevel=INFO --schedule /var/run/celery/kernelci-beat.db --app=taskqueue
 && python server.py
+
+# Celery beat currently not started 
+# && celery beat --loglevel=INFO --schedule /var/run/celery/kernelci-beat.db --app=taskqueue
