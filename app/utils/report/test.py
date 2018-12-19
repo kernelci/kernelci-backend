664960396977bb9b1274117a9bc1109076e0cfc3
Ana Guerrero Lopez <ana.guerrero@collabora.com>
2018-12-19 18:52:43 +0100

    utils/report: update test reports with regressions
    Update the reports to include next to the failed test cases the last
    kernel where they worked.
    
    Signed-off-by: Ana Guerrero Lopez <ana.guerrero@collabora.com>

--- app/utils/report/test.py
+++ app/utils/report/test.py
@@ -56,6 +56,112 @@ TEST_REPORT_FIELDS = [
 ]
 
 
+def _get_case(list_cases, name):
+    """Look for a test case with a given name
+
+    :param list_case: List of test cases from  test group
+    :type list_case: list
+    :param name: Name of a test case
+    :type name: string
+    """
+
+    for d in list_cases:
+        if d['name'] == name:
+            return d
+
+
+def create_regressions_data(test_docs, test_data):
+    """Create the regressions data for the email report.
+
+    Will create a dictionary with all the regressions
+
+    :param test_docs: The regressions data (the list of tests).
+    :type test_docs: list
+    :param test_data: Details about the test results
+    :type test_data: dict
+    :return dict The regressions results in a dictionary.
+    """
+    regr_dict = {}
+
+    # Make sure the test reports in the regressions data structure are
+    # correctly sorted by date, so that the oldest document is the PASS one
+    # and the most recent one is the last FAIL-ed.
+    test_docs.sort(
+        cmp=lambda x, y: cmp(x[models.CREATED_KEY], y[models.CREATED_KEY]))
+
+    ndocs = len(test_docs)
+    last_good = test_docs[0]
+
+    # Check case by case when was the first time it failed
+    for tc in last_good["test_cases"]:
+        for i in range(ndocs-1, 0, -1):
+            post = test_docs[i]
+            prev = test_docs[i-1]
+            post_case = _get_case(post["test_cases"], tc["name"])
+            prev_case = _get_case(prev["test_cases"], tc["name"])
+
+            if prev_case["status"] == ['PASS'] and post_case["status"] == 'FAIL':
+                msg = "last worked in"
+                d = {tc["name"]: "{} {}".format(msg, prev["git_describe"])}
+                regr_dict.update(d)
+                continue
+
+            if prev_case["status"] == ['SKIP'] and post_case["status"] == 'FAIL':
+                msg = "new FAIL, skipped in"
+                d = {tc["name"]: "{} {}".format(msg, prev["git_describe"])}
+                regr_dict.update(d)
+                continue
+
+    return regr_dict
+
+
+def parse_regressions(lab_regressions, group_data):
+    """Parse the regressions data and create a dictionary with the
+    regressions for the report. The dictionary has the test name as key
+    and the last kernel were it worked as value.
+
+    The returned data structure is:
+
+        {
+            "data": {
+                 'test name 1': 'v4.20-rc4-301-g13294fbcd4c1',
+                 'test name 2': 'v4.20-rc4-300-g34caf313b358',
+                 ...
+            }
+        }
+
+    :param lab_regressions: The regressions data for each lab.
+    :type lab_regressions: dict
+    :param group_data: Details about the test results
+    :type group_data: dict
+    :return dict The regressions data structure for the report.
+    """
+    regressions = {}
+    lab_name = group_data.get("lab_name", None)
+    arch_name = group_data.get("arch", None)
+    board_name = group_data.get("board", None)
+
+    for lab, lab_d in lab_regressions.iteritems():
+        if lab_name and lab != lab_name:
+            continue
+
+        for arch, arch_d in lab_d.iteritems():
+            if arch_name and arch_name != arch:
+                continue
+
+            for board, board_d in arch_d.iteritems():
+                if board_name and board_name != board:
+                    continue
+
+                for instance_d in board_d.itervalues():
+                    for defconfig, defconfig_d in instance_d.iteritems():
+                        for compiler, tests in defconfig_d.iteritems():
+                            regr = create_regressions_data(tests, group_data)
+                            regressions.update({"data": regr})
+
+    return regressions
+
+
 def _add_test_group_data(group, database):
     test_cases = []
     for test_case_id in group[models.TEST_CASES_KEY]:
@@ -81,11 +187,24 @@ def _add_test_group_data(group, database):
         for status, count in sub_group_total.iteritems():
             total[status] += count
 
+    # Check regressions
+    reg_doc = database[models.TEST_GROUP_REGRESSIONS_COLLECTION].find_one(
+        {models.JOB_KEY: group[models.JOB_KEY],
+         models.KERNEL_KEY: group[models.KERNEL_KEY],
+         models.GIT_BRANCH_KEY: group[models.GIT_BRANCH_KEY],
+         models.NAME_KEY: group[models.NAME_KEY]})
+
+    if reg_doc:
+        regressions = parse_regressions(reg_doc[models.REGRESSIONS_KEY], group)
+    else:
+        regressions = None
+
     group.update({
         "test_cases": test_cases,
         "sub_groups": sub_groups,
         "total_tests": sum(total.values()),
         "total": total,
+        "regressions": regressions,
     })
 
 
@@ -151,7 +270,10 @@ def create_test_report(data, email_format, db_options,
         subject_str = "Test results for {}/{} - {}".format(job, branch, kernel)
     else:
         plans_string = ", ".join(plans)
-        subject_str = "Test results ({}) for {}/{} - {}".format(plans_string, job, branch, kernel)
+        subject_str = "Test results ({}) for {}/{} - {}".format(plans_string,
+                                                                job,
+                                                                branch,
+                                                                kernel)
 
     git_url, git_commit = (top_groups[0][k] for k in [
         models.GIT_URL_KEY, models.GIT_COMMIT_KEY])
