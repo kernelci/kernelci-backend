b6c2b038c05c717428348f30b6d0127d8bcb643e
Matt Hart <matthew.hart@linaro.org>
2019-09-10 14:51:13 +0100

    Change all source code to LGPL v2.1+
    Also update all committers copyright on each file header.

--- app/utils/lava_log_parser.py
+++ app/utils/lava_log_parser.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
+# Copyright (C) Collabora Limited 2017,2018,2019
+# Author: Guillaume Tucker <guillaume.tucker@collabora.com>
+# Author: Ana Guerrero Lopez <ana.guerrero@collabora.com>
+# Author: Guillaume Tucker <guillaume@mangoz.org>
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Based on log2html.py:
-# https://github.com/khilman/lab/blob/master/log2html.py
-#
-# Error/warning formatting, css, colors etc. stolen from
-# Olof Johansson <olof@lixom.net>
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import argparse
 import cgi
