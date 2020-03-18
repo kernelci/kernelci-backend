a94603a230045ba84edbe931774dbbb65f6eaa40
Guillaume Tucker <guillaume.tucker@collabora.com>
2020-03-18 22:15:13 +0000

    Add test_group.modules and .modules_dir fields
    Add .modules and .modules_dir fields to TestGroupDocument and populate
    their values from the associated build document to be able to tell
    which modules were used in tests.  Update unit tests accordingly.
    
    Signed-off-by: Guillaume Tucker <guillaume.tucker@collabora.com>

--- app/models/tests/test_test_group_model.py
+++ app/models/tests/test_test_group_model.py
@@ -68,6 +68,8 @@ class TestTestGroupModel(unittest.TestCase):
         test_group.kernel = "kernel"
         test_group.kernel_image = "kernel-image"
         test_group.mach = "mach"
+        test_group.modules = "moduli"
+        test_group.modules_dir = "moduli-dir"
         test_group.parent_id = "parent-id"
         test_group.plan_variant = "tother"
         test_group.sub_groups = [True, False]
@@ -111,6 +113,8 @@ class TestTestGroupModel(unittest.TestCase):
             "kernel_image": "kernel-image",
             "lab_name": "lab-name",
             "mach": "mach",
+            "modules": "moduli",
+            "modules_dir": "moduli-dir",
             "parent_id": "parent-id",
             "plan_variant": "tother",
             "name": "name",
@@ -172,6 +176,8 @@ class TestTestGroupModel(unittest.TestCase):
             "kernel_image": None,
             "lab_name": "lab-name",
             "mach": "mach",
+            "modules": "modules",
+            "modules_dir": "modules_dir",
             "name": "name",
             "parent_id": "parent-id",
             "plan_variant": "tother",
